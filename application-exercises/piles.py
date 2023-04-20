f099120d316748d2770f08395a7c72d06e30961a
kingsleyabonyi <kinabonyi@gmail.com>
2023-04-20 06:59:50 +0100

    piles

---
+++ application-exercises/piles.py
@@ -0,0 +1,23 @@
+from karel.stanfordkarel import *
+
+# File: piles.py
+# -----------------------------
+# The warmup program defines a "main"
+# function which should make Karel
+# pick up all the beepers in the world.
+def main():
+    while front_is_clear():
+        move()
+        pickbeeper()
+    
+        
+def pickbeeper():
+    while beepers_present():
+        pick_beeper()
+        
+
+   
+# don't edit these next two lines
+# they tell python to run your main function
+if __name__ == '__main__':
+    main()
